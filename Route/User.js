14738e9fae331145a95c644a993a332e65773304
kyaalods <108167252+kyaalods@users.noreply.github.com>
2023-02-23 10:25:36 +0800

    Update User.js
    Version 0.0.39
    - Google request minor improvements
    - Adjusted Titan account session to 25 threads
    - Fixed Titan Draw Store Selection and minor improvements
    - Fixed CW Launches and replaced keywords to SKU.

--- Route/User.js
+++ Route/User.js
@@ -214,7 +214,7 @@ router.post('/reset-license', auth, (req, res)=>{
 })
 
 router.get('/HRB/version', (req,res)=>{
-    res.json({'version': '0.0.38'})
+    res.json({'version': '0.0.39'})
 })
 // router.post('/key', (req, res)=>{
 //         const key = req.body.key
