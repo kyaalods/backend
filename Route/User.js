03f9d2c48ba98e4945b077e2d8ccf9a8130f3886
kyaalods <108167252+kyaalods@users.noreply.github.com>
2023-03-31 12:12:27 +0800

    Fixed Smticket & Launches

--- Route/User.js
+++ Route/User.js
@@ -214,7 +214,7 @@ router.post('/reset-license', auth, (req, res)=>{
 })
 
 router.get('/HRB/version', (req,res)=>{
-    res.json({'version': '0.0.45'})
+    res.json({'version': '0.0.46'})
 })
 // router.post('/key', (req, res)=>{
 //         const key = req.body.key
