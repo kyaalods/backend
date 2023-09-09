420f3e31f60696f7bad1ef102093bfb5d82e94d3
kyaalods <108167252+kyaalods@users.noreply.github.com>
2023-09-09 11:28:44 +0800

    Update User.js
    - Disabled errorWebhook to lessen discord rate limit
    - Fixed issue with park access statistics for total entries
    - Fixed minor issues

--- Route/User.js
+++ Route/User.js
@@ -214,7 +214,7 @@ router.post('/reset-license', auth, (req, res)=>{
 })
 
 router.get('/HRB/version', (req,res)=>{
-    res.json({'version': '0.0.65'})
+    res.json({'version': '0.0.66'})
 })
 // router.post('/key', (req, res)=>{
 //         const key = req.body.key
