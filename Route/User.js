36a9bf8dfd955b49b289afcd35df58d7d85afdfd
kyaalods <108167252+kyaalods@users.noreply.github.com>
2023-11-04 19:10:27 +0800

    Update User.js
    0.0.72
    - Updated Footlocker Email Subscription
    - Added Pulptickets Account Gen
    - Fix Parkaccess Raffle Menu

--- Route/User.js
+++ Route/User.js
@@ -212,7 +212,7 @@ router.post('/reset-license', auth, (req, res)=>{
 })
 
 router.get('/HRB/version', (req,res)=>{
-    res.json({'version': '0.0.71'})
+    res.json({'version': '0.0.72'})
 })
 // router.post('/key', (req, res)=>{
 //         const key = req.body.key
