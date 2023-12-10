7690eb5ac0683751cbde4f84366fa75c939ebe7c
kyaalods <108167252+kyaalods@users.noreply.github.com>
2023-12-10 10:44:17 +0800

    Update User.js
    - Fix Parkaccess live raffle selection error
    - Initial support for Titan RSVP(BETA) SKU is not yet working, the default SKU is RSRV

--- Route/User.js
+++ Route/User.js
@@ -212,7 +212,7 @@ router.post('/reset-license', auth, (req, res)=>{
 })
 
 router.get('/HRB/version', (req,res)=>{
-    res.json({'version': '0.0.75'})
+    res.json({'version': '0.0.76'})
 })
 // router.post('/key', (req, res)=>{
 //         const key = req.body.key
