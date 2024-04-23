26a3d783e6ad316b8cb1b411bbd10416dea1d77b
kyaalods <108167252+kyaalods@users.noreply.github.com>
2024-04-24 06:14:52 +0800

    Update User.js

--- Route/User.js
+++ Route/User.js
@@ -211,7 +211,7 @@ router.post('/reset-license', auth, (req, res)=>{
 })
 
 router.get('/HRB/version', (req,res)=>{
-    res.json({'version': '0.0.89'})
+    res.json({'version': '0.0.90'})
 })
 
 router.get('/HRB/Beta/version', (req,res)=>{
