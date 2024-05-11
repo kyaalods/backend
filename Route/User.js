e82c9fe1c7ec8261a277b8dc3bb0db0d1d793efb
kyaalods <108167252+kyaalods@users.noreply.github.com>
2024-05-11 15:28:38 -0600

    Update User.js
    Update Titan Proxy 
    Updated Shopify account reset and recovery

--- Route/User.js
+++ Route/User.js
@@ -211,7 +211,7 @@ router.post('/reset-license', auth, (req, res)=>{
 })
 
 router.get('/HRB/version', (req,res)=>{
-    res.json({'version': '0.0.95'})
+    res.json({'version': '0.0.96'})
 })
 
 router.get('/HRB/Beta/version', (req,res)=>{
