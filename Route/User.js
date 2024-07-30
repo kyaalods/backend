14ef662bd1900fa9ce1fbfc69004265d22b44e38
kyaalods <108167252+kyaalods@users.noreply.github.com>
2024-07-30 08:07:11 -0600

    Update User.js
    - updated account gen menu for niek

--- Route/User.js
+++ Route/User.js
@@ -215,11 +215,11 @@ router.get('/HRB/version', (req,res)=>{
 })
 
 router.get('/HRB/windows/version', (req,res)=>{
-    res.json({'version': '0.5.4'})
+    res.json({'version': '0.5.5'})
 })
 
 router.get('/HRB/mac/version', (req,res)=>{
-    res.json({'version': '0.5.4'})
+    res.json({'version': '0.5.5'})
 })
 // router.post('/key', (req, res)=>{
 //         const key = req.body.key
