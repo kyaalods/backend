babaeeca7660e43ebb3c78bfa4e85f6a4ef9ae14
kyaalods <108167252+kyaalods@users.noreply.github.com>
2024-10-01 20:07:41 -0600

    Update User.js
    - Updated Niek account gen, activate, email changer OTP delay. It will now retry to get the latest OTP code for 4 times with 4 sec delay.
    - Fixed Titan userwebhook Os version.

--- Route/User.js
+++ Route/User.js
@@ -215,7 +215,7 @@ router.get('/HRB/version', (req,res)=>{
 })
 
 router.get('/HRB/windows/version', (req,res)=>{
-    res.json({'version': '0.7.7'})
+    res.json({'version': '0.7.8'})
 })
 
 router.get('/HRB/mac/version', (req,res)=>{
