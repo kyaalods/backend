0affd3bdb8119011b2fe91e8becb5b9fba725e35
kyaalods <108167252+kyaalods@users.noreply.github.com>
2024-12-10 20:09:58 -0700

    Update User.js
    - Possible fixed for titan raffle draw getting ip issue.
    - Fixed fetching live raffle.

--- Route/User.js
+++ Route/User.js
@@ -215,7 +215,7 @@ router.get('/HRB/version', (req,res)=>{
 })
 
 router.get('/HRB/windows/version', (req,res)=>{
-    res.json({'version': '0.8.3'})
+    res.json({'version': '0.8.4'})
 })
 
 router.get('/HRB/mac/version', (req,res)=>{
