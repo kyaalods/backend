0c79022e6a943515f8a84df49dd4b07ea5693d67
kyaalods <108167252+kyaalods@users.noreply.github.com>
2025-01-03 17:19:46 -0700

    Update User.js
    0.8.6
    - SNKRS EL improvements. It will now generate early links for all available products.

--- Route/User.js
+++ Route/User.js
@@ -215,7 +215,7 @@ router.get('/HRB/version', (req,res)=>{
 })
 
 router.get('/HRB/windows/version', (req,res)=>{
-    res.json({'version': '0.8.5'})
+    res.json({'version': '0.8.6'})
 })
 
 router.get('/HRB/mac/version', (req,res)=>{
