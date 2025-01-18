4275004d381bb957e87d513ed761ad62e9fef3b1
kyaalods <108167252+kyaalods@users.noreply.github.com>
2025-01-17 20:46:50 -0700

    Update User.js
    - Added ways to bypass waf

--- Route/User.js
+++ Route/User.js
@@ -223,7 +223,7 @@ router.get('/HRB/mac/version', (req,res)=>{
 })
 
 router.get('/Kurama/windows/version', (req,res)=>{
-    res.json({'version': '0.8'})
+    res.json({'version': '0.9'})
 })
 // router.post('/key', (req, res)=>{
 //         const key = req.body.key
