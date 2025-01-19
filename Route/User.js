b3447f7c23704c058c7a233d6e556262ef66f3ff
kyaalods <108167252+kyaalods@users.noreply.github.com>
2025-01-18 20:50:39 -0700

    Update User.js
    - Fixed version endpoint

--- Route/User.js
+++ Route/User.js
@@ -222,11 +222,11 @@ router.get('/HRB/mac/version', (req,res)=>{
     res.json({'version': '0.7.6'})
 })
 
-router.get('/SNKRS/windows/version', (req,res)=>{
+router.get('/Kurama/windows/version', (req,res)=>{
     res.json({'version': '0.9'})
 })
 
-router.get('/Kurama/windows/version', (req,res)=>{
+router.get('/SNKRS/windows/version', (req,res)=>{
     res.json({'version': '0.0.1'})
 })
 // router.post('/key', (req, res)=>{
