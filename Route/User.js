f5445b606d5c14e665423e47ec4ac857530d792c
kyaalods <108167252+kyaalods@users.noreply.github.com>
2025-02-13 19:37:24 -0700

    Update User.js
    - Added better gift shop international raffle
    - Titan on-demand CF improvements. 
    - Updated Niek akamai harvester and ksd update

--- Route/User.js
+++ Route/User.js
@@ -215,7 +215,7 @@ router.get('/HRB/version', (req,res)=>{
 })
 
 router.get('/HRB/windows/version', (req,res)=>{
-    res.json({'version': '0.9.0'})
+    res.json({'version': '0.9.1'})
 })
 
 router.get('/HRB/mac/version', (req,res)=>{
