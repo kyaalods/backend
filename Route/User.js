69313ebba29682481a1bfc83b18443f4a14051b8
kyaalods <108167252+kyaalods@users.noreply.github.com>
2025-03-11 10:02:35 -0600

    Update User.js
    - Fixed Niek account gen random email 
    - Update Niek account activate not skipping activated account.

--- Route/User.js
+++ Route/User.js
@@ -215,7 +215,7 @@ router.get('/HRB/version', (req,res)=>{
 })
 
 router.get('/HRB/windows/version', (req,res)=>{
-    res.json({'version': '0.9.5'})
+    res.json({'version': '0.9.6'})
 })
 
 router.get('/HRB/mac/version', (req,res)=>{
