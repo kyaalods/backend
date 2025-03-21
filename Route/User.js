b7d47a5995a7e4ed00ad6ec0dde276e0dd8c23c2
kyaalods <108167252+kyaalods@users.noreply.github.com>
2025-03-20 18:34:17 -0600

    Update User.js
    - Added Ticket Master Queue Handler
    - ESEM minor adjustments
    - Ticketnet proxy adjustment. If you happen to encounter any error carting ticket, please empty your proxies.txt.

--- Route/User.js
+++ Route/User.js
@@ -223,7 +223,7 @@ router.get('/HRB/mac/version', (req,res)=>{
 })
 
 router.get('/Kurama/windows/version', (req,res)=>{
-    res.json({'version': '1.5'})
+    res.json({'version': '1.6'})
 })
 
 router.get('/SNKRS/windows/version', (req,res)=>{
