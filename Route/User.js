5c628ea88bc0e26b90ac4aa6724581aa77deda52
kyaalods <108167252+kyaalods@users.noreply.github.com>
2025-04-22 21:17:42 -0600

    Update User.js
    - Added Credit Card Code support
    - Fixed saving queue token
    - Fixed loading queue token

--- Route/User.js
+++ Route/User.js
@@ -223,7 +223,7 @@ router.get('/HRB/mac/version', (req,res)=>{
 })
 
 router.get('/Kurama/windows/version', (req,res)=>{
-    res.json({'version': '1.7'})
+    res.json({'version': '1.8'})
 })
 
 router.get('/SNKRS/windows/version', (req,res)=>{
