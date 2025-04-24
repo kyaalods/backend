53bc22604dff23578e9f61606d197a9863a25d8a
kyaalods <108167252+kyaalods@users.noreply.github.com>
2025-04-23 21:42:59 -0600

    Update User.js
    - Fixed ticketnet queue handler
    - error logging improvements
    - some minor improvements

--- Route/User.js
+++ Route/User.js
@@ -223,7 +223,7 @@ router.get('/HRB/mac/version', (req,res)=>{
 })
 
 router.get('/Kurama/windows/version', (req,res)=>{
-    res.json({'version': '1.8'})
+    res.json({'version': '1.9'})
 })
 
 router.get('/SNKRS/windows/version', (req,res)=>{
