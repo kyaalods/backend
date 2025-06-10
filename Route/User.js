78b7818ccfa16df1012b0662a4c8d94a1f71d1c7
kyaalods <108167252+kyaalods@users.noreply.github.com>
2025-06-09 22:12:23 -0600

    Update User.js
    - fixed bug with esem getting other event information during fetching event data

--- Route/User.js
+++ Route/User.js
@@ -223,7 +223,7 @@ router.get('/HRB/mac/version', (req,res)=>{
 })
 
 router.get('/Kurama/windows/version', (req,res)=>{
-    res.json({'version': '1.11'})
+    res.json({'version': '1.12'})
 })
 
 router.get('/SNKRS/windows/version', (req,res)=>{
