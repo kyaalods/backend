9ed608c86bd80b33d8d3fa3a4d60ca44c6c67b44
kyaalods <108167252+kyaalods@users.noreply.github.com>
2025-06-11 10:55:47 -0600

    Update User.js
    1.14
    - Queue Handler automatically adapt challenges endpoints
    
    - Queue Handler improvements
    
    - Fixed ESEM Task cookie not found / Proxy banned error.

--- Route/User.js
+++ Route/User.js
@@ -223,7 +223,7 @@ router.get('/HRB/mac/version', (req,res)=>{
 })
 
 router.get('/Kurama/windows/version', (req,res)=>{
-    res.json({'version': '1.13'})
+    res.json({'version': '1.14'})
 })
 
 router.get('/SNKRS/windows/version', (req,res)=>{
