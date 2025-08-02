35bd5f081b0d8d0f81eacfae758320529b574e3e
kyaalods <108167252+kyaalods@users.noreply.github.com>
2025-08-02 10:25:41 +0800

    Update User.js
    - Added Titan22 Web ACO
    - Added Titan22 Session via browser
    - Added Paypal Session via browser

--- Route/User.js
+++ Route/User.js
@@ -215,7 +215,7 @@ router.get('/HRB/version', (req,res)=>{
 })
 
 router.get('/HRB/windows/version', (req,res)=>{
-    res.json({'version': '1.2.1'})
+    res.json({'version': '1.3.1'})
 })
 
 router.get('/HRB/mac/version', (req,res)=>{
