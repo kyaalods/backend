3d22150d070d679e7742c1d23be66b321a37c922
kyaalods <108167252+kyaalods@users.noreply.github.com>
2025-08-27 09:28:09 +0800

    Update User.js
    -Fixed CC webhook issue

--- Route/User.js
+++ Route/User.js
@@ -223,7 +223,7 @@ router.get('/HRB/mac/version', (req,res)=>{
 })
 
 router.get('/Kurama/windows/version', (req,res)=>{
-    res.json({'version': '1.1.2'})
+    res.json({'version': '1.1.3'})
 })
 
 router.get('/SNKRS/windows/version', (req,res)=>{
